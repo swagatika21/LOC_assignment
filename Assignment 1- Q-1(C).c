35ab49b76c77d643589669cfc82508ef0548395c
Suchismitapanda123 <99337131+Suchismitapanda123@users.noreply.github.com>
2022-02-13 12:06:13 +0530

    Suchismita Panda

---
+++ Assignment 1- Q-1(C).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+
+{
+printf("Name: SUCHISMITA PANDA\n");
+Printf("Regd no.: 077\n");
+printf("Branch: Eletrical Engineering\n)");
+printf("Hobbies: Singing , Dancing\n");
+getchar();
+}
+ 
+    
